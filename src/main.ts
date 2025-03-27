ab1aa2426b5c2731e356d4a6d18540206a28471d
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-27 17:43:42 +0530

    added astricks in main

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://timehub.chaincodeconsulting.com'],
+    origin: ['http://localhost:3000', 'https://timehub.chaincodeconsulting.com','*'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    // credentials: true,
     preflightContinue: false, // Ensures the browser doesn't block preflight
   });
   
