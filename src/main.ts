69cc1d84f3fbb388065433c5aa220ea54a4818a7
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-27 17:30:43 +0530

    preflight-false

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://timehub.chaincodeconsulting.com'], // Allow specific domains
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: ['http://localhost:3000', 'https://timehub.chaincodeconsulting.com'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
+    preflightContinue: false, // Ensures the browser doesn't block preflight
   });
+  
   
   
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
