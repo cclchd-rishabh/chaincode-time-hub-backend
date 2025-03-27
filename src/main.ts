66050b9f618f9828cd19dafea8560f870b48bac9
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-27 17:19:57 +0530

    allowed all origin in cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors();
+  app.enableCors({
+    origin: '*',  // Change this to your frontend domain in production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // If using cookies or authentication
+  });
+  
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   await app.listen(4000);
