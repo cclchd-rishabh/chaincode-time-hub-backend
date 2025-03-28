7c8df1bffd08de5f8430833f5689c17898c09605
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-28 10:40:50 +0530

    updated-options in auth-controllers

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://timehub.chaincodeconsulting.com'],
+    origin: [
+      'http://localhost:3000', 
+      'https://timehub.chaincodeconsulting.com',
+      'http://localhost:3008'  // Add if needed
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    allowedHeaders: [
+      'Content-Type', 
+      'Authorization', 
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Methods',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Credentials'
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 
   await app.listen(3008);
