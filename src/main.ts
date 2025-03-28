2254dcdd483d403e1d842d55d0b129c288cae17a
Rishabh <cclchd-rishabh@Rishabhs-MacBook-Air.local>
2025-03-28 12:00:37 +0530

    added useStatic assest to serve image in forntend

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // ✅ Proper CORS Configuration (Avoids Multiple Values Issue)
+
   app.enableCors({
-    origin: 'https://timehub.chaincodeconsulting.com', // Allow only your frontend
+    origin: 'https://timehub.chaincodeconsulting.com', // Allow only your frontend in production
+    // origin: 'http://localhost:3000',
     credentials: true, // Allow cookies/session auth
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads', // Access files via http://localhost:3008/uploads/filename.png
   });
 
   await app.listen(3008);
